7f1840dfa622dec655f2a5ecbbf9cb25ee4707d1
Inaya-TheCoder <inaya.singh2013@gmail.com>
2025-11-13 21:11:40 +0530

    homework

--- astris pyramid.py
+++
@@ -1,5 +0,0 @@
-a = int(input("Enter the no. of row you want:"))
-for i in range (a):
- for j in range(i + 1):
-  print ("* ",end="")
- print()
---
+++ homework.py
@@ -0,0 +1,7 @@
+a = int(input("Enter the no. of rows:"))
+b = input("enter the symbol")
+for i in range (1+a):
+    print(i)
+for i in range(n, 0, -1):
+        print('* ' * +i)
+
